dddd990e7c499ee27930e15c1f5f1b7c2c184ceb
LucaPoltiGit <luca.tobias.polti@gmail.com>
2024-05-27 22:54:00 -0300

    Inicializamos el proyecto

---
+++ dbConnection/dbConnection.js
@@ -0,0 +1,22 @@
+import { Sequelize } from "sequelize";
+import {
+  DB_NAME,
+  DB_USER,
+  DB_PASSWORD,
+  DB_HOST,
+  DB_DIALECT,
+  DB_PORT,
+} from "../config/config.js";
+const dbConnection = new Sequelize(DB_NAME, DB_USER,DB_PASSWORD, {
+  host:  DB_HOST,
+  dialect: DB_DIALECT,
+  port:  DB_PORT,
+});
+
+try {
+  await dbConnection.authenticate();
+  console.log("Connection has been established successfully.");
+} catch (error) {
+  console.error("Unable to connect to the database:", error);
+}
+export default dbConnection;
