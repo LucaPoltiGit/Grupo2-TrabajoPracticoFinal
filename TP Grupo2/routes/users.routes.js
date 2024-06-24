36e847e0782c9afc1e32293bb17084df5e5517bf
IairFei <feigelmaniair@gmail.com>
2024-06-24 16:26:54 -0300

    Implementando logout

--- TP Grupo2/routes/users.routes.js
+++ TP Grupo2/routes/users.routes.js
@@ -1,6 +1,5 @@
 import { Router } from "express";
 import userController from "../controller/userController.js";
-import { generateToken } from "../utils/jwt.js";
 
 const router = Router();
 
@@ -50,37 +49,6 @@ router.post("/", async (req, res) => {
     }
 })
 
-router.post("/login", async (req, res) => {
-    try {
-        const {email, password} = req.body
-
-        const user = await userController.getByEmail(email)
-console.log(user)
-        if(!user) {
-            return res.status(404).json({status: "Error", msg: `Usuario con el email ${email} no se encontro`})
-        }
-
-        const comparePassword = await user.validatePassword(password)
-
-        if(!comparePassword) {
-            return res.status(404).json({status: "Error", msg: `El usuario o contraseña es incorrecto`})
-        }
-
-        const payload = {
-            id: user.id,
-            name: user.name
-        }
-
-        const token = generateToken(payload)
-
-        res.cookie("token", token)
-
-        res.status(200).json({status: "success", message: "Usuario logueado correctamente"});
-    } catch (error) {
-        res.status(500).json({ status: "Error", msg: "Error interno del servidor" });
-        console.log(error.message);
-    }
-})
 
 router.put("/:uid", async (req, res) => {
     try {
